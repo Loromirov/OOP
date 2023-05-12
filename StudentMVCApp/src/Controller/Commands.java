d8c9f28f8ca2c331edefa18222c8cf7732627eb1
Loromirov <Loromirov@yandex.ru>
2023-05-12 14:09:14 +0300

    homework5

---
+++ StudentMVCApp/src/Controller/Commands.java
@@ -0,0 +1,12 @@
+package Controller;
+
+/** Cписок команд для взаимодействия с пользователем */
+public enum Commands {
+    NONE,
+    READ,
+    CREATE,
+    UPDATE,
+    LIST,
+    DELETE,
+    EXIT
+}
